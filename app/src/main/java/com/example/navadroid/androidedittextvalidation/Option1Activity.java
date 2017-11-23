176d137efbaaa3805bc6cf68c2697e27df3d989e
Pailin <klaw.amz@gmail.com>
2017-11-23 22:22:31 +0700

    Do option1 but doesn't check password.

--- app/src/main/java/com/example/navadroid/androidedittextvalidation/Option1Activity.java
+++ app/src/main/java/com/example/navadroid/androidedittextvalidation/Option1Activity.java
@@ -2,6 +2,7 @@ package com.example.navadroid.androidedittextvalidation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -23,18 +24,18 @@ public class Option1Activity extends AppCompatActivity {
         initView();
     }
 
-    private void bindView(){
+    private void bindView() {
         etName = (EditText) findViewById(R.id.et_name1);
         etPwd = (EditText) findViewById(R.id.et_pwd1);
         etEmail = (EditText) findViewById(R.id.et_email1);
         etPhone = (EditText) findViewById(R.id.et_phone1);
     }
 
-    private void initView(){
+    private void initView() {
         findViewById(R.id.btn_validate1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(validateEditText()) {
+                if (validateEditText()) {
                     Toast.makeText(Option1Activity.this, "Okay. You are good to go.", Toast.LENGTH_SHORT).show();
                     // SnackBar?
                 }
@@ -55,6 +56,7 @@ public class Option1Activity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 validateEditText(); // OR validation can be specific (only for this EditText)
+                validateName();
             }
         });
 
@@ -72,6 +74,43 @@ public class Option1Activity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 // TODO: add your Password validation here
+
+                // Password can be anything
+
+            }
+        });
+
+        etPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                validatePhone();
+
+            }
+        });
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                validateEmail();
             }
         });
 
@@ -80,14 +119,83 @@ public class Option1Activity extends AppCompatActivity {
     }
 
     // To validate all EditTexts
-    private boolean validateEditText(){
+    private boolean validateEditText() {
         boolean isValidated = true;
         if (etName.getText().toString().length() == 0) {
             etName.setError("Required");
             isValidated = false;
+        } else if (etPwd.getText().toString().length() == 0) {
+            etPwd.setError("Required");
+            isValidated = false;
+        } else if (etEmail.getText().toString().length() == 0) {
+            etEmail.setError("Required");
+            isValidated = false;
+        } else if (etPhone.getText().toString().length() == 0) {
+            etPhone.setError("Required");
+            isValidated = false;
         }
         // TODO: add your EditText validation here
-
         return isValidated;
+    }
+
+
+    // To validate name
+    private boolean validateName() {
+        boolean nameIsValidated = true;
+        String text = etName.getText().toString();
+        if (!text.matches("^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$")) {
+            nameIsValidated = false;
+            etName.setError("Invalid format");
+        }
+
+        return nameIsValidated;
+    }
+
+    // To validate password
+
+//    ^                 # start-of-string
+//            (?=.*[0-9])       # a digit must occur at least once
+//            (?=.*[a-z])       # a lower case letter must occur at least once
+//            (?=.*[A-Z])       # an upper case letter must occur at least once
+//            (?=.*[@#$%^&+=])  # a special character must occur at least once you can replace with your special characters
+//            (?=\\S+$)          # no whitespace allowed in the entire string
+//            .{4,}             # anything, at least six places though
+//    $                 # end-of-string
+
+
+    //Mai check ka check tam mai ka
+    private boolean validatePwd() {
+        boolean pwdIsValidated = true;
+        String text = etPwd.getText().toString();
+        if (!text.matches("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\\\S+$).{8,}")) {
+            pwdIsValidated = false;
+            etPwd.setError("Invalid format");
+        }
+
+        return pwdIsValidated;
+    }
+
+    // To validate email
+    private boolean validateEmail() {
+        boolean emailIsValidated = true;
+        String text = etEmail.getText().toString();
+        if (!text.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+            emailIsValidated = false;
+            etEmail.setError("Invalid format");
+        }
+
+        return emailIsValidated;
+    }
+
+    // To validate phone number
+    private boolean validatePhone() {
+
+        boolean phoneIsValidated = true;
+        String text = etPhone.getText().toString();
+        if (!text.matches("^(?:0091|\\\\+91|0)[7-9][0-9]{7,8}$")) {
+            phoneIsValidated = false;
+            etPhone.setError("Invalid format");
+        }
+        return phoneIsValidated;
     }
 }
